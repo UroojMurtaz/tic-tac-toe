51133a2c8daf4fbe1428069ce5a32613351cbfbd
comsats <uroojmurtaza12@gmail.com>
2021-10-27 14:12:46 +0500

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View,Button } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View > 
+
+  <View style ={styles.paragraph}> 
+  
+  <View  style={{flexDirection:"row",justifyContent:"space-between",margin:10}}> 
+   <Button title=""  /> 
+   <Button title="" /> 
+  <Button title="" />
+
+  </View>
+  <View style={{flexDirection:"row",justifyContent:"space-between",margin:10}}> 
+  <Button title=""  />
+  <Button title=""  />
+  <Button title=""  />
+
+  </View>
+  <View style={{flexDirection:"row",justifyContent:"space-between",margin:10}}> 
+  <Button title=""  />
+  <Button title="" />
+  <Button title=""  />
+  </View>
+  </View>
+  </View>
   );
 }
 
@@ -17,5 +37,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection :"row"
   },
+  paragraph: {
+    margin: 120,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    
+    
+  },
+  
 });
+
+
